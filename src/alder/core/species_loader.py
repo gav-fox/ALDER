b959145c270a9fa5f9b8d1581224f0719deb61bd
Gav Fox <Gavinhfox@gmail.com>
2026-07-04 12:52:59 +0100

    Loader now returns SpeciesRecord with common and latin name

--- src/alder/core/species_loader.py
+++ src/alder/core/species_loader.py
@@ -1,7 +1,7 @@
 import yaml 
+from alder.core.species_record import SpeciesRecord
 def load_species (file_path):
     with open (file_path, "r") as f:
         data = yaml.safe_load(f)
-    return data
-
-    
+    species = SpeciesRecord (data["common_name"], data["latin_name"])
+    return species
